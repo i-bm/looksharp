1403d3dc27759efb7aa5a84a0c414894432b5145
Isaac Boakye-Manu <isaac@Isaacs-MacBook-Pro.local>
2025-12-04 07:09:04 +0000

    added:opg description for landing page

--- app/Http/Controllers/Pages/HomeController.php
+++ app/Http/Controllers/Pages/HomeController.php
@@ -11,6 +11,6 @@ class HomeController extends Controller
         $title = 'Verified. Connected. Hired.';
         $description = 'Kickstart your career with Looksharp — Ghana’s trusted platform for verified internships, early-career jobs, and employer connections. Build your profile, get matched to real opportunities, and launch your future with confidence.';
 
-        return view('pages.home.index', compact('title'));
+        return view('pages.home.index', compact('title', 'description'));
     }
 }
